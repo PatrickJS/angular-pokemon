23b55e4cc4be1de0026eab0ab285725485afde37
gdi2290 <github@gdi2290.com>
2013-10-16 21:37:44 -0700

    add pokemon directive

--- angular-pokemon.js
+++ angular-pokemon.js
@@ -1,4 +1,21 @@
 (function(angular, undefined) {
 "use strict";
+
+angular.module('angular-pokemon', [])
+  .directive('pokemon', function() {
+    return {
+      link: function(scope, element, attrs) {
+        var img = angular.element('<img>');
+        element.html('');
+        element.append(img);
+        scope.$watch(attrs.ngPokemon, function (thatPokemon) {
+          if (thatPokemon) {
+            img.attr('src', 'http://img.pokemondb.net/artwork/' + thatPokemon + '.jpg');
+          }
+        })
+      }
+    }
+  })
+
 }(angular, undefined));
 
